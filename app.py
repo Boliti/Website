a70fd7b7d55bac986ca60df927dc62b6501ac1e1
Boliti <volanikita@gmail.com>
2025-09-15 22:04:29 +0400

    WebSite2.1.7Deep

--- app.py
+++ app.py
@@ -390,7 +390,7 @@ async def export_mean_spectrum(request: ExportMeanRequest):
 if __name__ == '__main__':
     import uvicorn, os
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="localhost", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
     
     
     
